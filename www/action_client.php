a649557b3bc37406c225f097732f06dd201c64d5
Alexey Erokhov <ean1@bk.ru>
2022-06-29 12:43:13 +0300

    реформат кода

--- www/action_client.php
+++ www/action_client.php
@@ -8,26 +8,27 @@ if (mysqli_connect_errno()) {
     printf("Connect failed: %s\n", mysqli_connect_error());
     exit();
 }
-$results = $conn->query("select * from crm_clients where server='".$_POST['server2']."'");
+$results = $conn->query("select * from crm_clients where server='" . $_POST['server2'] . "'");
 if (mysqli_num_rows != 0) {
-?>
-<link rel="stylesheet" href="css/style.css" type="text/css"/>
-<table><thead>
-    <tr>
-        <th>Номер клиента</th>
-        <th>Тип</th>
-    </tr>
-	</thead>
-<tbody>
-<? while ($data = $results->fetch_assoc()): ?>
-	<tr>
-		<td><? echo $data['num'] ?></td>
-		<td><? echo $data['server'] ?></td>
-	</tr>
+    ?>
+    <link rel="stylesheet" href="css/style.css" type="text/css"/>
+    <table>
+        <thead>
+        <tr>
+            <th>Номер клиента</th>
+            <th>Тип</th>
+        </tr>
+        </thead>
+        <tbody>
+        <? while ($data = $results->fetch_assoc()): ?>
+            <tr>
+                <td><? echo $data['num'] ?></td>
+                <td><? echo $data['server'] ?></td>
+            </tr>
 
-<? endwhile; ?>
-</tbody>
-</table>
-<? } else {?>
-<div class="bar error">Клиенты не найдены</div>
-<?}?>
+        <? endwhile; ?>
+        </tbody>
+    </table>
+<? } else { ?>
+    <div class="bar error">Клиенты не найдены</div>
+<? } ?>
--- www/action_order.php
+++ www/action_order.php
@@ -22,35 +22,36 @@ case
 	when status='2' then 'Принятие решения'
 	when status='3' then 'Утверждена'
 end as status
-from crm_orders where server='".$_POST['server2']."'");
+from crm_orders where server='" . $_POST['server2'] . "'");
 $count_result = mysqli_num_rows($results);
 
 echo $_POST['point_num'];
 echo $_POST['status'];
 ?>
-<div class="bar success">Количество найденных заявок: <?echo $count_result?></div>
+<div class="bar success">Количество найденных заявок: <? echo $count_result ?></div>
 <link rel="stylesheet" href="css/style.css" type="text/css"/>
-<table><thead>
+<table>
+    <thead>
     <tr>
         <th>Номер заявки</th>
         <th>Тип</th>
         <th>Статус</th>
-		<th>Номер точки</th>
-		<th>Сервер</th>
-		<th>Маршрут проверки</th>
+        <th>Номер точки</th>
+        <th>Сервер</th>
+        <th>Маршрут проверки</th>
     </tr>
-	</thead>
-<tbody>
-<? while ($data = $results->fetch_assoc()): ?>
-	<tr>
-		<td><? echo $data['num'] ?></td>
-		<td><? echo $data['type'] ?></td>
-		<td><? echo $data['status'] ?></td>
-		<td><? echo $data['point_num'] ?></td>
-		<td><? echo $data['server'] ?></td>
-		<td><? echo $data['route'] ?></td>
-	</tr>
+    </thead>
+    <tbody>
+    <? while ($data = $results->fetch_assoc()): ?>
+        <tr>
+            <td><? echo $data['num'] ?></td>
+            <td><? echo $data['type'] ?></td>
+            <td><? echo $data['status'] ?></td>
+            <td><? echo $data['point_num'] ?></td>
+            <td><? echo $data['server'] ?></td>
+            <td><? echo $data['route'] ?></td>
+        </tr>
 
-<? endwhile; ?>
-</tbody>
+    <? endwhile; ?>
+    </tbody>
 </table>
